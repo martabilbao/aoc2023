aed6eb51aa43d7b8f81f203dab07637209913219
martabilbao <marta@bilbaoareitio.es>
2023-12-02 10:41:01 +0100

    Initial setup

---
+++ day01/day01-part1.ts
@@ -0,0 +1,6 @@
+import path from "path";
+import { createInputFileReadLineInterface } from "../utils"
+
+const input = createInputFileReadLineInterface(path.join(__dirname, 'input.txt'));
+
+console.log("hola")
